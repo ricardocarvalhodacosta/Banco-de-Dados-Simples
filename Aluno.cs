0c44db6e9f2261c33348b1fa8d065fba49f15cba
Ricardo Carvalho da Costa <115588683+ricardocarvalhodacosta@users.noreply.github.com>
2023-04-24 18:19:56 -0300

    Atualizando os arquivos e criando novas Classes
    Foram adicionadas novas Classes para que o código ficasse mais limpo e bem dividido.

---
+++ Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoEscola
+{
+    internal class Aluno
+    {
+        public string Nome { get; set; } // propriedade para o nome do aluno
+        public float Nota1 { get; set; } // propriedade para a primeira nota do aluno
+        public float Nota2 { get; set; } // propriedade para a segunda nota do aluno
+        public float Media { get; set; } // propriedade para a média do aluno
+
+        public Aluno(string nome, float nota1, float nota2, float media) // construtor da classe Aluno
+        {
+            Nome = nome; // atribui o nome recebido à propriedade Nome
+            Nota1 = nota1; // atribui a primeira nota recebida à propriedade Nota1
+            Nota2 = nota2; // atribui a segunda nota recebida à propriedade Nota2
+            Media = (nota1 + nota2) / 2; // calcula a média e atribui o valor à propriedade Media
+        }
+    }
+}
---
+++ Sala.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoEscola
+{
+    internal class SalaDeAula
+    {
+        public string Nome { get; set; } // propriedade para o nome da sala de aula
+        public List<Aluno> Alunos { get; set; } // propriedade para a lista de alunos
+
+        public SalaDeAula(string nome)
+        {
+            Nome = nome; // define o nome da sala de aula
+            Alunos = new List<Aluno>(); // inicializa a lista de alunos
+        }
+
+        public void AdicionarAluno(Aluno aluno) // método para adicionar um aluno à lista
+        {
+            Alunos.Add(aluno);
+        }
+
+        public void RemoverAluno(Aluno aluno) // método para remover um aluno da lista
+        {
+            Alunos.Remove(aluno);
+        }
+
+        public void AtualizarAluno(Aluno aluno, string novoNome, float novaNota1, float novaNota2) // método para atualizar as informações de um aluno
+        {
+            aluno.Nome = novoNome; // atualiza o nome do aluno
+            aluno.Nota1 = novaNota1; // atualiza a nota 1 do aluno
+            aluno.Nota2 = novaNota2; // atualiza a nota 2 do aluno
+        }
+
+        // Método para salvar os dados da sala de aula em um arquivo de texto
+        public void SalvarDados()
+        {
+            string nomeArquivo = Nome + ".txt";  // Define o nome do arquivo como o nome da sala de aula + ".txt"
+
+            using (StreamWriter file = new StreamWriter(nomeArquivo))  // Cria um objeto StreamWriter para escrever no arquivo
+            {
+                foreach (Aluno aluno in Alunos)  // Percorre a lista de alunos da sala de aula
+                {
+                    file.WriteLine(aluno.Nome + ";" + aluno.Nota1 + ";" + aluno.Nota2);  // Escreve o nome e as notas do aluno separados por ";"
+                }
+            }
+        }
+    }
+}
