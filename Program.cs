5f6b182d9b4ad0bd5e24b1559abfcc0a03dc4484
Ricardo Carvalho da Costa <115588683+ricardocarvalhodacosta@users.noreply.github.com>
2023-04-24 18:30:44 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,18 @@
-﻿using BancoEscola;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using static BancoSimples.Interface1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Cria uma nova instância de FileManager
-        FileManager FileManager = new FileManager();
-
-        // Cria uma nova instância de Menu
-        Menu menu = new Menu();
-
-        // Exibe o menu de seleção de classe e lê a opção escolhida pelo usuário
-        menu.SelectClass();
-        menu.menu = char.Parse(Console.ReadLine().ToUpper());
-
-        // Chama o método SelectFuncion da instância de Menu, passando como argumento a instância de FileManager
-        menu.SelectFuncion(FileManager);
+        BancoDeDados bancoDeDados = new BancoDeDados();
+        InterfaceUsuario interfaceUsuario = new InterfaceUsuario(bancoDeDados);
+        interfaceUsuario.Executar();
+        Console.WriteLine("** DADOS ATUALIZADOS COM SUCESSO!! **");
+        Console.WriteLine();
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
     }
 }
